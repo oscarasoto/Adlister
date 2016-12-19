e55427560a80a652ad5ba9797f5c99a2df0615c0
Oscar Soto <oscarasotoa@gmail.com>
2016-12-19 11:50:31 -0600

    Make sure passwords are being hashed before they enter the database. Change logic to check against users' hashed passwords. Don't allow ad creation unless a user is logged in. When a new ad is created, assign the user id of the logged in user to the ad.

--- src/main/java/com/codeup/adlister/controllers/LoginServlet.java
+++ src/main/java/com/codeup/adlister/controllers/LoginServlet.java
@@ -2,6 +2,8 @@ package com.codeup.adlister.controllers;
 
 import com.codeup.adlister.dao.DaoFactory;
 import com.codeup.adlister.models.User;
+import com.codeup.adlister.util.Password;
+import org.mindrot.jbcrypt.BCrypt;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -30,10 +32,14 @@ public class LoginServlet extends HttpServlet {
             return;
         }
 
-        boolean validAttempt = password.equals(user.getPassword());
+        // Getting hashed password from DB.
+        String hashPassword = user.getPassword();
+        // Checking if password match against hashed stored password.
+        boolean validAttempt = Password.check(password, hashPassword);
 
         if (validAttempt) {
             request.getSession().setAttribute("user", user);
+            request.getSession().setAttribute("user_id", user.getId());
             response.sendRedirect("/profile");
         } else {
             response.sendRedirect("/login");
